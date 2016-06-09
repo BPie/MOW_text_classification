dbedea2847df3896c3e57ae194c8b8ad2772509c
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-09 23:56:24 +0200

    ADD setting column names

--- src/prepare_data.R
+++ src/prepare_data.R
@@ -1,6 +1,18 @@
-path_project = ".."
-path_data_root = paste(path_project,"datasets", sep="/")
-path_data_sms = paste(path_data_root, "sms.csv", sep="/")
+# setting paths
+path_project <-  ".."
+path_data_root  <- paste(path_project,"datasets", sep="/")
+path_data_sms <- paste(path_data_root, "sms.csv", sep="/")
 
-sms_raw = read.table(path_data_sms, sep="\t", quote="")
-sms_raw[1:5,]
+# reading data
+sms_raw <- read.table(path_data_sms
+                     , sep="\t"
+                     , quote=""
+                     , stringsAsFactors=FALSE
+                     , header=FALSE)
+
+# setting column names
+colnames(sms_raw)  <- c("type", "message")
+str(sms_raw[1:10,])
+
+
+
