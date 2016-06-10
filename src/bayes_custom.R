664034ec8cb5e63b201b4f4f5d2b1551c4ed26f7
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-10 17:11:00 +0200

    ADD computing priors

--- src/bayes_custom.R
+++ src/bayes_custom.R
@@ -4,12 +4,44 @@ if(!exists("learn_set"))
 }
 library("gmodels")
 
-myNaiveBayes  <- function(data, smoothing=0)
+myNaiveBayes  <- function(data, types, smoothing=0)
 {
+    types_lvls  <- levels(types)
+    # assumption, that we learn for the first class 
+    # (and all the rest)
+    target_type  <- types_lvls[1]
+
+    types_lvls_count <- length(types_lvls)
+    bow_count  <- NCOL(data) # bag of words count
+
+    types_summary  <- table(types)
+    data_len  <-  NROW(types)
+
+    # data initialization
+    priors <- data.frame(matrix(ncol=1
+                               , nrow=types_lvls_count))
+    row.names(priors)  <- types_lvls
+    names(priors)  <- c("prior")
+
+    likelihood <- data.frame(matrix(ncol=bow_count
+                                   , nrow=types_lvls_count))
+    row.names(likelihood) <- types_lvls
+    names(likelihood) <- names(data)
+
+    for(t in types_lvls)
+    {
+        # setting priors
+        priors[t, "prior"]  <- types_summary[t]/data_len
     
+        # setting approx. likelihood
+        
+    }
+    print(priors)
 }
 
 predict.myNaiveBayes  <- function(modelObject) 
 {
     
 }
+myNaiveBayes(learn_set, learn_types)
+
