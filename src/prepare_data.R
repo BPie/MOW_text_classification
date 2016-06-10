8e47f26da2d89572516c793284026b41e970c124
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-10 10:11:32 +0200

    ADD freq word

--- src/prepare_data.R
+++ src/prepare_data.R
@@ -9,6 +9,7 @@ path_data_sms <- paste(path_data_root, "sms.csv", sep="/")
 
 # setting consts
 TEST_SET_PART  <- 0.2
+WORD_MIN_FREQ  <- 0.001
 
 # reading data
 sms_raw <- read.table(path_data_sms
@@ -40,10 +41,20 @@ test_set_count  <- ceiling(set_count * TEST_SET_PART)
 learn_set_count  <- set_count - test_set_count
 
 test_idxs  <- 1:test_set_count
-learn_idxs  <-  test_set_count+1:set_count
+learn_idxs  <-  (test_set_count+1):(set_count)
 
 test_set  <- sms_dtm[test_idxs,]
 learn_set  <- sms_dtm[learn_idxs,]
 
 test_types <- sms_raw[test_idxs,]$type
 learn_types <- sms_raw[learn_idxs,]$type
+
+# finding frequent words
+word_count  <- length(dimnames(sms_dtm)$Terms)
+word_min_count  <-  ceiling(word_count * WORD_MIN_FREQ)
+frequent_words  <- findFreqTerms(learn_set, word_min_count)
+frequent_words_count  <- length(frequent_words)
+
+# narrowing dtm sets
+test_set  <- test_set[,frequent_words]
+learn_set  <- learn_set[,frequent_words]
