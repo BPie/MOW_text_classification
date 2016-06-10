f14b0b567dcb7b4b3836cbab9b0af2da92c86adf
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-10 12:55:30 +0200

    INIT custom bayes

---
+++ src/bayes_custom.R
@@ -0,0 +1,15 @@
+if(!exists("learn_set")) 
+{
+    source("prepare_data.R")
+}
+library("gmodels")
+
+myNaiveBayes  <- function(data, smoothing=0)
+{
+    
+}
+
+predict.myNaiveBayes  <- function(modelObject) 
+{
+    
+}
