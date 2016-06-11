4069369968e798948b177827b5396dcf1221aaf3
Maksymilian Mazur <maksymilian.mazur@lingaro.local>
2016-06-11 20:54:43 +0200

    Pushing intial version of the kNN native and custom. Custom tested

---
+++ src/classification_knn.R
@@ -0,0 +1,199 @@
+
+knnCustom.predict(trainSet, testSet, trainClasses, k = 1, simMeasure = 0, isKplus = FALSE, weights = c(1,1)) <- {
+  # Predicts the kNN classifier output
+  #
+  # Args:
+  #   trainSet: Training set 
+  #   testSet: Testing set
+  #   trainClasses: Labels of the training set
+  #   k: number of neighbours to obtains as voters for a class
+  #   simMeasure: 0: euclidean Distance, 1: cosine Dissimilarity
+  #   isKplus: if TRUE, knn+ is used
+  #   weights: constant weights for voting for a given class
+  #
+  # Returns:
+  #   The kNN model of classifier
+  
+  # Creating a model of a lazy classifier is useless and is only taking up the memory
+  # So the model is not created
+  # The prediction keeps its official form
+  
+  distanceMatrix <- knnCustom.dist(trainSet, testSet, simMeasure)
+  
+  # Generate the voteList of all the closestNeighbours
+  voteLists <- apply(distanceMatrix, 1,function(x){
+    if(isKplus) 
+      orderKPlus(x, k) 
+    else
+      order(x)[1:k]
+  })
+  
+  # Weighted votes Vectors
+  voteLists <- sapply(voteLists,function(x){
+    singleVote(x, weights, trainClasses)
+  })
+  
+  # votes
+  class <- apply(voteLists, 2, function(x){
+    order(x)[-1]
+  })
+  
+  knnCustom.predict <- class
+}
+
+knnCustom.predict <- function(distanceMatrix, trainClasses, k = 1, simMeasure = 0, isKplus = FALSE, weights = c(1,1)) {
+  # Predicts the kNN classifier output
+  #
+  # Args:
+  #   distanceMatrix: Pre-calculated distance matrix, where ROWS: represent testingSet and 
+  #                   columns represent trainingSet
+  #   k: number of neighbours to obtains as voters for a class
+  #   simMeasure: 0: euclidean Distance, 1: cosine Dissimilarity
+  #   isKplus: if TRUE, knn+ is used
+  #   weights: constant weights for voting for a given class
+  #
+  # Returns:
+  #   The kNN model of classifier
+  
+  # Generate the voteList of all the closestNeighbours
+  voteLists <- apply(distanceMatrix, 1,function(x){
+    if(isKplus) 
+      orderKPlus(x, k) 
+    else
+      order(x)[1:k]
+  })
+  
+  # Weighted votes Vectors
+  voteLists <- sapply(voteLists,function(x){
+    singleVote(x, weights, trainClasses)
+  })
+  
+  # votes
+  class <- apply(voteLists, 2, function(x){
+    order(x)[-1]
+  })
+
+  knnCustom.predict <- class
+}
+
+
+
+
+knn.p <- knnCustom.predict(z,c,1)
+
+knnCustom.dist <- function(trainSet, testSet, simMeasure = 0) {
+  # Computes the dissimilarity between two matrices
+  #
+  # Args:
+  #   trainSet: trainingsSet, to which distance is caluclated
+  #   testSet: testSet, to which distance is calculated
+  #   simMeasure: if 0: calculate euclidean, if 1: caluculate Cosine
+  #
+  # Returns:
+  #   The matrices of a-b elements distances
+  
+  # For all the samples in a testing set and all the samples in a training
+  # set, calculate distance
+  
+  if(!is.matrix(trainSet) || !is.matrix(testSet))
+    error("TrainSet or TestSet are not matrices")
+  else if(NCOL(trainSet)!=NCOL(testSet))
+        error("TrainSet and TestSet have different number of arguments")
+
+  distanceVector <- apply(trainSet, 1, function(x){
+    apply(testSet,1, function(y){
+      if(simMeasure == 0)
+        calculateEuclidean(x,y)
+      else
+        calculateCosine(x,y)
+    })
+  })
+  
+}
+
+calculateEuclidean <- function(a,b){
+  # Computes the euclidean dissimilarity (distance) between two vectors
+  #
+  # Args:
+  #   a: One of two vectors which are taken into consideration
+  #   b: Second of the vectors
+  #
+  # Returns:
+  #   The euclidean distance between vectors a and b (scalar)
+  
+  if(!is.vector(a) || !is.vector(b))
+    error("Input parameters must be one-dimensional arrays")
+  else if(length(a)!=length(b))
+    error("Input vectors are of different lengths in calculateEudclidean function")
+  
+  sum <- sum(mapply(function(x,y){(x-y)^2}, a, b))
+
+  calculateEuclidean <- sum^(1/2)
+  
+}
+
+calculateCosine <- function(a,b){
+  # Computes the cosine dissimilarity (distance) between two vectors
+  #
+  # Args:
+  #   a: One of two vectors which are taken into consideration
+  #   b: Second of the vectors
+  #
+  # Returns:
+  #   The cosine reverse similarity (1-cossim) between vectors a and b (scalar)
+  
+  if(!is.vector(a) || !is.vector(b))
+    error("Input parameters must be one-dimensional arrays")
+  else if(length(a)!=length(b))
+    error("Input vectors are of different lengths in calculateCosine function")
+
+  sumAB <- sum(mapply(function(x,y){x*y}, a, b))
+  sumA <- sum(sapply(a, function(x){x^2}))
+  sumB <- sum(sapply(b, function(x){x^2}))
+  
+  if(sumA!=0 && sumB!=0)
+    calculateCosine <- (1 - sumAB/(sumA*sumB)^(1/2))
+  else
+    calculateCosine <- 1
+}
+
+orderKPlus <- function(x, k) {
+  # Get K++ neighbours for a sample of the testSet
+  #
+  # Args:
+  #   x: all the trainingSet's distances
+  #   k: nominal k value
+  #
+  # Returns:
+  #   Array of K++ size containing k nearest neighbour and the equal-distanced to the 
+  #   most remote one
+  
+  ordered <- x[order(x)]
+  
+  i <- k
+  while(ordered[i]==ordered[k]){
+    print(ordered[i])
+    i <- i+1
+  }
+  
+  orderKPlus <- order(x)[1:i-1]
+}
+
+
+singleVote <- function(x, weights, trainClasses) {
+  # Counts the number of votes for each class based on weights and vector of labels of the training set
+  #
+  # Args:
+  #   x: Array of distances of a sample from all trainig samples
+  #   weights: An Array of weights for all the classes
+  #   trainClasses:  Vector of labels of the training Set
+  #
+  # Returns:
+  #   Array of votes for each class
+  
+  singleVote <- sapply (1:length(weights), function(y){
+    sum(weights[y]*as.numeric(trainClasses[x]==y))
+  } )
+  
+}
+
---
+++ src/classification_knn_native.R
@@ -0,0 +1,73 @@
+library(tm) 
+library(class) 
+library(SnowballC) 
+
+setwd("C:/Users/usr/Desktop/MOW_text_classification")
+
+##########################################
+# MOW Classification kNN native          #
+# Date: 2015-06-08                       #
+# By: Rozalia Pietrzak                   #
+##########################################
+
+# setting consts
+TEST_SET_PART  <- 0.2
+WORD_MIN_FREQ  <- 0.001
+
+
+# Read data
+mainDF <- read.csv("./datasets/sms.csv", 
+               sep ="\t", 
+               header = FALSE, 
+               stringsAsFactors = FALSE, 
+               quote = "")
+
+colnames(mainDF) <- c("class", "text")
+
+
+# Generate corpus out of the text-column and
+# Clean corpus (since we will be doing
+# Binary Term, Frequent Term and TF-IDF,
+# it is better to clean the corpus than
+# to clean each of the sets)
+corpus <- Corpus(VectorSource(mainDF$text))
+
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, stemDocument, language = "english")
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+
+# Create DocumentTerm Frequency Matrix, with frequent, binary and TF-IDF
+frequentDTM <- DocumentTermMatrix(corpus)
+#binaryDTM <- DocumentTermMatrix(corpus, control = list(weighting =
+#                                                        function(x)
+#                                                        weightBin(x)))
+#tfidfDTM <- DocumentTermMatrix(corpus, control = list(weighting =
+#                                                         function(x)
+#                                                           weightTfIdf(x), normalization=FALSE))
+
+# Transform dtm to matrix to data frame - df is easier to work with
+frequentDF <- as.data.frame(data.matrix(frequentDTM))
+frequentDF <- cbind(frequentDF, mainDF$class)
+#binaryDF <- as.data.frame(data.matrix(binaryDTM))
+#tfidfDF <- as.data.frame(data.matrix(tfidfDTM))
+
+# For deterministic comparison, use split in half
+trainSet <- frequentDF[1:(NROW(frequentDF)*4/5-1),1:(NCOL(frequentDF)-1)]
+testSet <- frequentDF[(NROW(frequentDF)*4/5):(NROW(frequentDF)-1),1:(NCOL(frequentDF)-1)]
+cl <- frequentDF[1:(NROW(frequentDF)*4/5-1),NCOL(frequentDF)-1]
+clTest <-  frequentDF[(NROW(frequentDF)*4/5):(NROW(frequentDF)-1),NCOL(frequentDF)-1]
+
+# finding frequent words
+frequentWords  <- findFreqTerms(frequentDTM, (NCOL(frequentDF)-1)*WORD_MIN_FREQ)
+trainSet <- trainSet[,frequentWords]
+testSet <- testSet[,frequentWords]
+
+# Classifier
+knnpred <- knn(trainSet, testSet, cl, k = 1, l = 0, prob = FALSE)
+
+# Confusion matrix
+confusionMat <- table(knn.pred, clTest)
+confusionMat
