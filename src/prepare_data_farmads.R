b1b9f3d8bd9153658667adc089b63c7837656837
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-11 02:09:53 +0200

    INIT file, small REF

---
+++ src/prepare_data_farmads.R
@@ -0,0 +1,10 @@
+library(tm)
+library(SnowballC)
+librart(RSofia)
+
+# setting paths
+path_project <-  ".."
+path_data_root  <- paste(path_project,"datasets", sep="/")
+path_data_sms <- paste(path_data_root, "farm.txt", sep="/")
+
+
