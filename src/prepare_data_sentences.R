5aca29c35f837615be008b49e842a2115bbcbe19
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-11 21:14:06 +0200

    ADD final preparation (frequent words, learning/test test)

--- src/prepare_data_sentences.R
+++ src/prepare_data_sentences.R
@@ -62,32 +62,36 @@ articles_dtm  <- DocumentTermMatrix(articles_corpus
                                                      , stemming=FALSE))
 
 
-# reading data
-#raw_data <- read.svmlight(path_data_farmads)
-#data  <- raw_data$data
-#types  <- raw_data$labels
+# splitting data
+set_count <- NROW(articles_dtm)
+test_set_count <- ceiling(set_count * TEST_SET_PART)
+learn_set_count <- set_count - test_set_count
 
-## splitting data
-#set_count <- NROW(data)
-#test_set_count <- ceiling(set_count * TEST_SET_PART)
-#learn_set_count <- set_count - test_set_count
+test_idxs <- 1:test_set_count
+learn_idxs <- (test_set_count+1):(set_count)
 
-#test_idxs  <- 1:test_set_count
-#learn_idxs  <- (test_set_count+1):(set_count)
+test_set <- articles_dtm[test_idxs,]
+learn_set <- articles_dtm[learn_idxs,]
 
-#test_set  <- data[test_idxs,]
-#learn_set  <- data[learn_idxs,]
+test_types <- all_articles[test_idxs,]$type
+learn_types <- all_articles[learn_idxs,]$type
 
-#test_types <- types[test_idxs]
-#learn_types <- types[learn_idxs]
+# finding frequent words
+word_count  <- length(dimnames(articles_dtm)$Terms)
+word_min_count  <-  ceiling(word_count * WORD_MIN_FREQ)
+frequent_words  <- findFreqTerms(learn_set, word_min_count)
+frequent_words_count  <- length(frequent_words)
 
+# narrowing dtm sets
+test_set  <- test_set[,frequent_words]
+learn_set  <- learn_set[,frequent_words]
 
-## convert to bag of words
-#conv_to_bow <- function(inp) inp <- ifelse(inp > 0, "Yes", "No");
-#test_set  <- apply(test_set
-                   #, MARGIN=2
-                   #, conv_to_bow)
+# convert to bag of words
+conv_to_bow <- function(inp) inp <- ifelse(inp > 0, "Yes", "No");
+test_set  <- apply(test_set
+                   , MARGIN=2
+                   , conv_to_bow)
 
-#learn_set  <- apply(learn_set
-                   #, MARGIN=2
-                   #, conv_to_bow)
+learn_set  <- apply(learn_set
+                   , MARGIN=2
+                   , conv_to_bow)
