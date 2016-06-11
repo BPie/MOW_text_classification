547ebf6c8587abf963a8f099b4e2f5700ec65270
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-06-11 11:28:56 +0200

    ADD reading

--- src/prepare_data_farmads.R
+++ src/prepare_data_farmads.R
@@ -1,10 +1,15 @@
 library(tm)
 library(SnowballC)
-librart(RSofia)
+library(RSofia)
 
 # setting paths
 path_project <-  ".."
 path_data_root  <- paste(path_project,"datasets", sep="/")
-path_data_sms <- paste(path_data_root, "farm.txt", sep="/")
+path_data_farmads <- paste(path_data_root, "farm.txt", sep="/")
 
-
+raw_data <- read.svmlight(path_data_farmads)
+data  <- raw_data$data
+labels  <- raw_data$labels
+data[1:5,1:19]
+NROW(data)
+NCOL(data)
